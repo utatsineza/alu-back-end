78b5ddef23bf253b2266df9613b6c61c14101c5f
utatsineza <h.utatsinez@alustudent.com>
2024-10-18 21:49:46 -0700

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,7 +10,6 @@ import sys
 
 if __name__ == '__main__':
     user_id = sys.argv[1]
-
 user_url = "https://jsonplaceholder.typicode.com/users/{}" \
         .format(user_id)
     todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
